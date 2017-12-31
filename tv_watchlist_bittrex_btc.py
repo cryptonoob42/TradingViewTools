c6c4f0af485c74d11ae2881251ef447e976fea57
cryptonoob42 <30018991+cryptonoob42@users.noreply.github.com>
2017-12-31 11:19:24 +0100

    Add a simple script to create a Bittrex BTC markets watchlist
    The script uses the public API of Bittrex to retrieve existing
    markets, picks out those where the base currency is BTC and
    creates a TradingView watchlist which is then printed to the console.

---
+++ tv_watchlist_bittrex_btc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import urllib2
+import json
+
+try:
+	result = urllib2.urlopen("https://bittrex.com/api/v1.1/public/getmarkets")
+	if result.getcode() == 200:
+		markets = json.loads(result.read())
+		if markets['success'] != True:
+			print "Markets were retrieved, but the success flag is false."
+			pass
+
+		tv_symbols = []
+		for market in markets['result']:
+			if market['BaseCurrency'] != 'BTC':
+				continue
+
+			tv_symbols.append('BITTREX:' + market['MarketCurrency'] + 'BTC')
+
+		tv_symbols.sort()
+		print ','.join(tv_symbols)
+	else:
+		print "Error retrieving markets. HTTP result: ", result.getcode()
+except urllib2.URLError:
+	print "Error retrieving markets."
+
