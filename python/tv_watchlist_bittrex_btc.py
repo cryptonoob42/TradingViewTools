4503b0b6c1f058419a9f231ccc8b619cf4ae474d
cryptonoob42 <30018991+cryptonoob42@users.noreply.github.com>
2018-01-02 21:07:12 +0100

    Replace tabs by spaces

--- python/tv_watchlist_bittrex_btc.py
+++ python/tv_watchlist_bittrex_btc.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python
 
-import urllib2
 import json
+import urllib2
 
 try:
-	result = urllib2.urlopen('https://bittrex.com/api/v1.1/public/getmarkets')
-	if result.getcode() == 200:
-		markets = json.loads(result.read())
-		if markets['success'] != True:
-			print 'Markets were retrieved, but the success flag is false.'
-			pass
+    result = urllib2.urlopen('https://bittrex.com/api/v1.1/public/getmarkets')
+    if result.getcode() == 200:
+        markets = json.loads(result.read())
+        if markets['success'] != True:
+            print 'Markets were retrieved, but the success flag is false.'
+            pass
 
-		tv_symbols = []
-		for market in markets['result']:
-			if market['BaseCurrency'] != 'BTC':
-				continue
+        tv_symbols = []
+        for market in markets['result']:
+            if market['BaseCurrency'] != 'BTC':
+                continue
 
-			tv_symbols.append('BITTREX:' + market['MarketCurrency'] + 'BTC')
+            tv_symbols.append('BITTREX:' + market['MarketCurrency'] + 'BTC')
 
-		tv_symbols.sort()
-		print ','.join(tv_symbols)
-	else:
-		print 'Error retrieving markets. HTTP result: ', result.getcode()
+        tv_symbols.sort()
+        print ','.join(tv_symbols)
+    else:
+        print 'Error retrieving markets. HTTP result: ', result.getcode()
 except urllib2.URLError:
-	print 'Error retrieving markets.'
-
+    print 'Error retrieving markets.'
